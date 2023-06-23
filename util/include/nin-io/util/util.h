9b71f8ed2f5e1a2fa4eb38e5ef39c86537566080
Ryan Brown <ryanbrown4610@gmail.com>
2023-06-23 11:55:42 -0700

    Switching to only ainb support, basic structure

--- util/include/nin-io/util/util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-
-int convertHexCharArrayToInt(char* array, bool isBigEndian, int len);
-void displayCharArrayAsHex(std::ostream& os, char* array, int len);
-void readIntFromStream(std::istream& stream, bool isBigEndian, int size, int& result);
-char* rawToHex(const char* rawData, int length);
