a526dabbac227d14a484a91b3612b5c363d79521
Ryan Brown <ryanbrown4610@gmail.com>
2023-07-29 20:52:33 -0700

    Removing c# wrapper, may implement in the future Also adding premake support

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "ainb"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"src/include/**.h"
+	}
+
+	includedirs
+	{
+		"src/include"
+	}
+
+	defines
+	{
+		"AINB_STATIC_DEFINE"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
